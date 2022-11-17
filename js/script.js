5cf7dcbb9974bf44a00a9f6189721f1ce81cee67
ramroman163 <=>
2022-11-17 01:42:33 -0300

    mejora

--- js/script.js
+++ js/script.js
@@ -1,25 +1,89 @@
+var listaCiudadesIguales = [];
+
 var form = document.getElementById("search-form");
 form.addEventListener("submit", onSubmit, true);
 
 function onSubmit(event) {
   event.preventDefault();
-  var search = document.getElementById("search-box");
-  buscar_ciudad(search.value);
-  form.reset();
-  document.activeElement?.blur();
+  var busqueda = document.getElementById("search-box");
+  main(busqueda.value);
+  resetCiudades();
 }
 
-async function buscar_ciudad(search) {
-  let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + search + ',AR&lang=sp&appid=5fab11b8cdf9affc7e1236fe4909ad05&units=metric'
+function main(busqueda) {
+  buscar_ciudad(busqueda);
+  // verificarCiudad();
+  document.activeElement?.blur();
+  form.reset();
+}
+
+async function buscar_ciudad(busqueda) {
+  let url = 'http://api.openweathermap.org/geo/1.0/direct?q=' + busqueda + ',AR&limit=7&lang=sp&appid=5fab11b8cdf9affc7e1236fe4909ad05'
   fetch(url)
     // Exito
     .then(response => response.json())  // convertir a json
-    .then(json => mostrar_datos_pantalla(json))    //imprimir los datos en la consola
+    .then(json => transformarALista(json))    //imprimir los datos en la consola
     .catch(err => console.log('Solicitud fallida', err));
 }
 
+/*
+async function buscar_clima(busqueda) {
+  let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + busqueda + ',AR&lang=es&appid=5fab11b8cdf9affc7e1236fe4909ad05&units=metric'
+  fetch(url)
+    // Exito
+    .then(response => response.json())  // convertir a json
+    .then(json => mostrarDatos(json))    //imprimir los datos en la consola
+    .catch(err => console.log('Solicitud fallida', err));
+}
+*/
 
-function mostrar_datos_pantalla(json) {
+function transformarALista(json) {
+  for (let i = 0; i < json.length; i++) {
+    listaCiudadesIguales.push(json[i]);
+  }
+  // console.log("NUEVA CIUDAD");
+  // for(let i=0; i<listaCiudadesIguales.length; i++){
+  //   console.log(listaCiudadesIguales[i]);
+  // }
+  // console.log(listaCiudadesIguales.length);
+
+  let cantidadCiudades = listaCiudadesIguales.length;
+  crearElementosLista(cantidadCiudades);
+  //crearElementosLista(ciudadesIguales);
+}
+/*
+function verificarCiudad() {
+  // if(listaCiudadesIguales.length == 1){
+  //   document.querySelector(".contenedor-datos").style.display = "flex";
+  //   buscar_clima(listaCiudadesIguales[0]["name"]);
+  // }
+  // else if(listaCiudadesIguales[0]["state"] == listaCiudadesIguales[1]["state"]){
+  //   document.querySelector(".contenedor-datos").style.display = "flex";
+  //   buscar_clima(listaCiudadesIguales[0]["name"]);
+  // }
+  // else{
+    crearElementosLista();
+  //}
+}*/
+
+function crearElementosLista(cantidadCiudades) {
+  // console.log("ENTRE A ELEMENTOSLISTA");
+  // console.log("LARGO CIUDADES: ");
+  // console.log(cantidadCiudades);
+  for (let i=0; i<cantidadCiudades; i++) {
+    let elementoLista = document.getElementById("lista-ciudades");
+    // console.log("ENTRE AL FOR");
+    let locacion = listaCiudadesIguales[i]["name"] + ", " + listaCiudadesIguales[i]["state"];
+    let li = document.createElement("li");
+    let liText = document.createTextNode(locacion);
+    li.appendChild(liText);
+    elementoLista.appendChild(li);
+  }
+
+}
+
+
+function mostrarDatos(json) {
   let city = json["name"];
 
   let time = json["dt"];
@@ -74,7 +138,6 @@ function mostrar_datos_pantalla(json) {
 function getDatetime(dt) {
   let date = new Date(dt * 1000);
 
-
   let datetime = (correctDatetime(date.getDate()) +
     "/" + (correctDatetime(date.getMonth() + 1)) +
     "/" + correctDatetime(date.getFullYear()) +
@@ -101,3 +164,14 @@ window.addEventListener("load", () => {
   document.querySelector(".loader").style.opacity = 0;
   document.querySelector(".loader").style.pointerEvents = "none";
 })
+
+function resetCiudades() {
+
+  var elementoLista = document.getElementById("lista-ciudades");
+
+  while (elementoLista.firstChild) {
+      elementoLista.removeChild(elementoLista.firstChild);
+  }
+  
+  listaCiudadesIguales = [];
+}
